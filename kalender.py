459329407ae877d5861791670f2e267e53a5f7ac
Your NameCewin <cewin.eriksson@elev.ga.ntig.se>
2022-09-07 15:29:24 +0200

    dagsslut 07-09

--- kalender.py
+++ kalender.py
@@ -12,16 +12,18 @@ today = datetime.date.today()
 window = Tk()
  
 # sätter storleken på fönstret
-window.geometry("500x500")
+window.geometry("500x700")
 
 # ändrar titeln på fönstret
 window.title("Kalender v0.01")
+
+inp=""
 
 # skapar kalendern och sätter startdatumet
 cal = Calendar(window, selectmode="day", year=today.year, month=today.month, day=today.day)
 cal.pack(pady=20, fill="both", expand=True)
 
-Text = tkinter.Canvas(window, width = 430, height = 330)
+Text = tkinter.Canvas(window, width = 500, height = 700)
 
 def grab_date():
     my_label.config(text= "Dagens datum är: "+cal.get_date())
@@ -32,27 +34,53 @@ def grab_date():
     NewWindow.geometry("200x300")
 
     # text för händelseruta
-    NewLabel = Label(NewWindow, text="Vad vill du skriva?")
+    NewLabel = Label(NewWindow, text="Händelse: ")
     NewLabel.pack()
-
-    #Ruta att skriva i
-    e = Entry(NewWindow)
-    e.pack()
 
     #Vad som händer när man klickar på knappen
     def myClick():
         myLabel = Label(NewWindow, text="Den funkade!!!")
         myLabel.pack()
 
+    def printInput():
+        global inp 
+        inp = inputtxt.get(1.0, "end-1c")
+
+        lbl = Label(NewWindow, text = "")
+        lbl.pack()
+        with open("event.txt", "a+") as file_object:
+            data = file_object.read(10000)
+            # Append text at the end of file
+            file_object.write("\n" + "Time: " + cal.get_date() + " event: " + inp)
+    # TextBox Creation
+    inputtxt = tkinter.Text(NewWindow, height = 5, width = 20)
+    inputtxt.pack()
+
     #Knapp att klicka på
-    myButton = Button(NewWindow, text="Klicka här", command=myClick)
+    myButton = Button(NewWindow, text = "Print", command = printInput)
     myButton.pack()
 
 my_button = Button(window, text = "Get Date", command = grab_date)
 my_button.pack(pady = 20)
 
+def buttonClick():
+    print("test")
+    with open("event.txt") as f:
+        next(f)
+        for line in f:
+            #do something
+            lines = f.readline()
+            datumLabel.config(text = lines)
+
+
 my_label = Label(window, text = "")
 my_label.pack(pady = 20)
+
+datumknapp = Button(window, text = "Visa händelser", command = buttonClick)
+datumknapp.pack(pady = 20)
+
+datumLabel = Label(window, text = "")
+datumLabel.pack()
 
 """events={'2022-09-28':('London','meeting'),\
     '2022-08-15':('Paris','meeting'),\
